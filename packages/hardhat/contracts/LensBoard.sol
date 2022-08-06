dc9c0b5ab47b46f34a43c17a91ae86f354a1b5ae
fraserdscott <fraserdscott@gmail.com>
2022-08-06 18:44:53 +0100

    Simplify contract to use Lens Dispatcher

--- packages/hardhat/contracts/LensBoard.sol
+++ packages/hardhat/contracts/LensBoard.sol
@@ -1,23 +1,20 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 import "@aave/lens-protocol/contracts/interfaces/ILensHub.sol";
 
-contract LensBoard is ERC721Holder, Ownable {
-    address public lensHub;
+contract LensBoard {
+    ILensHub public lensHub;
     address public collectModule;
     bytes private constant collectModuleInitData = abi.encode(true);
 
-    constructor(address _lensHub, address _collectModule) {
+    constructor(ILensHub _lensHub, address _collectModule) {
         lensHub = _lensHub;
         collectModule = _collectModule;
     }
 
     function post(uint256 profileId, string memory contentURI) public {
-        ILensHub(lensHub).post(
+        lensHub.post(
             DataTypes.PostData(
                 profileId,
                 contentURI,
@@ -27,9 +24,5 @@ contract LensBoard is ERC721Holder, Ownable {
                 ""
             )
         );
-    }
-
-    function transferProfile(address to, uint256 profileId) public onlyOwner {
-        IERC721(lensHub).safeTransferFrom(address(this), to, profileId);
     }
 }
--- packages/hardhat/test/myTest.js
+++ packages/hardhat/test/myTest.js
@@ -30,33 +30,5 @@ describe("My Dapp", function () {
         "0x0000000000000000000000000000000000000000"
       );
     });
-
-    describe("transferProfile()", function () {
-      it("Should be able to receive profile NFT's", async function () {
-        const [owner] = await ethers.getSigners();
-
-        expect(await lensHub.transferFrom(owner.address, myContract.address, 0))
-          .to.emit(lensHub, "Transfer")
-          .withArgs(owner.address, myContract.address, 0);
-      });
-
-      it("Should not be able to transfer profile out if not owner", async function () {
-        const [owner, addr1] = await ethers.getSigners();
-
-        await expect(
-          myContract.connect(addr1).transferProfile(owner.address, 0)
-        ).to.revertedWith("Ownable: caller is not the owner");
-      });
-
-      it("Should be able to transfer profile out", async function () {
-        const [owner] = await ethers.getSigners();
-
-        expect(
-          await myContract.connect(owner).transferProfile(owner.address, 0)
-        )
-          .to.emit(lensHub, "Transfer")
-          .withArgs(myContract.address, owner.address, 0);
-      });
-    });
   });
 });
